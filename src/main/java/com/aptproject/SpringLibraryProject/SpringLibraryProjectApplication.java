3eb6fe470cffdd5441da4f368987684d97cdd3d8
sencerrrrr <vakhitovdaniyar@mail.ru>
2024-10-24 11:43:27 +0300

    init

--- src/main/java/com/aptproject/SpringLibraryProject/SpringLibraryProjectApplication.java
+++ src/main/java/com/aptproject/SpringLibraryProject/SpringLibraryProjectApplication.java
@@ -1,5 +1,7 @@
 package com.aptproject.SpringLibraryProject;
+import com.aptproject.SpringLibraryProject.dbexample.dao.BookDAOBean;
 import com.aptproject.SpringLibraryProject.dbexample.dao.BookDaoJDBC;
+import com.aptproject.SpringLibraryProject.dbexample.db.DBConnection;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,12 +11,14 @@ public class SpringLibraryProjectApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLibraryProjectApplication.class, args);
-
 	}
 	@Override
 	public void run(String... args) throws Exception{
-		BookDaoJDBC bookDaoJDBC = new BookDaoJDBC();
-		bookDaoJDBC.findBookById(1);
+//		BookDaoJDBC bookDaoJDBC = new BookDaoJDBC();
+//		bookDaoJDBC.findBookById(1);
+
+		BookDAOBean bookDAOBean = new BookDAOBean(DBConnection.INSTANCE.getConnection());
+		bookDAOBean.findBookById(4);
 	}
 
 }
