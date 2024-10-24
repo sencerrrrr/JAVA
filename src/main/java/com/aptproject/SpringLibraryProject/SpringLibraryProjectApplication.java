0b840fecf87e2256cfa31bd1bdc1d013557e0135
sencerrrrr <vakhitovdaniyar@mail.ru>
2024-10-24 13:23:46 +0300

    init

--- src/main/java/com/aptproject/SpringLibraryProject/SpringLibraryProjectApplication.java
+++ src/main/java/com/aptproject/SpringLibraryProject/SpringLibraryProjectApplication.java
@@ -2,11 +2,14 @@ package com.aptproject.SpringLibraryProject;
 import com.aptproject.SpringLibraryProject.dbexample.dao.BookDAOBean;
 import com.aptproject.SpringLibraryProject.dbexample.dao.BookDaoJDBC;
 import com.aptproject.SpringLibraryProject.dbexample.db.DBConnection;
+import com.aptproject.SpringLibraryProject.dbexample.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
+import java.awt.*;
 import java.util.List;
 
 @SpringBootApplication
@@ -30,7 +33,7 @@ public class SpringLibraryProjectApplication implements CommandLineRunner {
 //		BookDaoJDBC bookDaoJDBC = new BookDaoJDBC();
 //		bookDaoJDBC.findBookById(1);
 
-		BookDAOBean bookDAOBean = new BookDAOBean(DBConnection.INSTANCE.getConnection());
+//		BookDAOBean bookDAOBean = new BookDAOBean(DBConnection.INSTANCE.getConnection());
 //		bookDAOBean.findBookById(4);
 		List<Book> bookList = jdbcTemplate.query("select * from books",
 				((rs, rowNum) -> new Book(
