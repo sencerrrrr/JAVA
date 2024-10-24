fff579d1785ca57c3d78d532a16bd85b39a567b4
sencerrrrr <vakhitovdaniyar@mail.ru>
2024-10-24 11:45:27 +0300

    init

--- src/main/java/com/aptproject/SpringLibraryProject/SpringLibraryProjectApplication.java
+++ src/main/java/com/aptproject/SpringLibraryProject/SpringLibraryProjectApplication.java
@@ -20,5 +20,4 @@ public class SpringLibraryProjectApplication implements CommandLineRunner {
 		BookDAOBean bookDAOBean = new BookDAOBean(DBConnection.INSTANCE.getConnection());
 		bookDAOBean.findBookById(4);
 	}
-
 }
