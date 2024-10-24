54e411a562536c589265dafdc06402a1be909ad7
sencerrrrr <vakhitovdaniyar@mail.ru>
2024-10-24 13:18:35 +0300

    init

--- src/main/java/com/aptproject/SpringLibraryProject/SpringLibraryProjectApplication.java
+++ src/main/java/com/aptproject/SpringLibraryProject/SpringLibraryProjectApplication.java
@@ -2,9 +2,12 @@ package com.aptproject.SpringLibraryProject;
 import com.aptproject.SpringLibraryProject.dbexample.dao.BookDAOBean;
 import com.aptproject.SpringLibraryProject.dbexample.dao.BookDaoJDBC;
 import com.aptproject.SpringLibraryProject.dbexample.db.DBConnection;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringLibraryProjectApplication implements CommandLineRunner {
@@ -15,6 +18,10 @@ public class SpringLibraryProjectApplication implements CommandLineRunner {
 		this.bookDAOBean = bookDAOBean;
 	}
 
+	@Autowired
+	private NamedParameterJdbcTemplate jdbcTemplate;
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLibraryProjectApplication.class, args);
 	}
@@ -24,6 +31,14 @@ public class SpringLibraryProjectApplication implements CommandLineRunner {
 //		bookDaoJDBC.findBookById(1);
 
 		BookDAOBean bookDAOBean = new BookDAOBean(DBConnection.INSTANCE.getConnection());
-		bookDAOBean.findBookById(4);
+//		bookDAOBean.findBookById(4);
+		List<Book> bookList = jdbcTemplate.query("select * from books",
+				((rs, rowNum) -> new Book(
+						rs.getInt("id"),
+						rs.getString("title"),
+						rs.getString("author"),
+						rs.getDate("date_added")
+				)));
+		bookList.forEach(System.out::println);
 	}
 }
