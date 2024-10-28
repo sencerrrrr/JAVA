d8dbdb5e25ef78d949a87af3b8cf87fe13e844bb
sencerrrrr <vakhitovdaniyar@mail.ru>
2024-10-28 11:09:13 +0300

    init

--- src/main/java/com/aptproject/SpringLibraryProject/library/model/BookRentInfo.java
+++ src/main/java/com/aptproject/SpringLibraryProject/library/model/BookRentInfo.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 @SequenceGenerator(name = "default_generator", sequenceName = "book_rent_seq_info", allocationSize = 1)
 
 public class BookRentInfo extends GenericModel {
-    @ManyToMany (cascade = {CascadeType.PERSIST, CascadeType.MERGE}) //у одного пользователя могут быть много арент
+    @ManyToOne (cascade = {CascadeType.PERSIST, CascadeType.MERGE}) //у одного пользователя могут быть много арент
     @JoinColumn(name = "user_id", nullable = false,
         foreignKey = @ForeignKey(name = "FK_RENT_BOOK_INFO_USER"))
     private User user;
